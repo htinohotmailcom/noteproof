afcf49ecc3a1f51c4e20ade52f0c2e01885de67a
htinohotmailcom <htino@hotmail.com>
2026-03-22 17:02:32 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.json());
+app.use(express.json({ type: '*/*' }));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.post('/api/generate', async (req, res) => {
   try {
